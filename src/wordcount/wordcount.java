b637e5933c9a30fb95d10ff5aa209d31a22a0eb1
minhonline2k2 <minhonline2k2@gmail.com>
2026-09-13 20:46:36 +0700

    change class name
    Renamed class from 'WordCount' to 'wordcount' and adjusted references.

--- src/wordcount/wordcount.java
+++ src/wordcount/wordcount.java
@@ -1,6 +1,5 @@
 import java.io.IOException;
 import java.util.StringTokenizer;
-
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
@@ -11,16 +10,11 @@ import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
-
-public class WordCount {
-
-
+public class wordcount {
     public static class TokenizerMapper
             extends Mapper<Object, Text, Text, IntWritable> {
-
         private final static IntWritable one = new IntWritable(1);
         private Text word = new Text();
-
         public void map(Object key, Text value, Context context)
                 throws IOException, InterruptedException {
             StringTokenizer itr = new StringTokenizer(value.toString());
@@ -30,13 +24,9 @@ public class WordCount {
             }
         }
     }
-
-
     public static class IntSumReducer
             extends Reducer<Text, IntWritable, Text, IntWritable> {
-
         private IntWritable result = new IntWritable();
-
         public void reduce(Text key, Iterable<IntWritable> values, Context context)
                 throws IOException, InterruptedException {
             int sum = 0;
@@ -47,7 +37,6 @@ public class WordCount {
             context.write(key, result);
         }
     }
-
     public static void main(String[] args) throws Exception {
         Configuration conf = new Configuration();
         Job job = Job.getInstance(conf, "word count");
